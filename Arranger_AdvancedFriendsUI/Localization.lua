f2e9bc063d31b0420a09c77eb246a7673410d908
Nikita S. Doroshenko <nikita@s.doroshenko.im>
2017-09-09 14:53:57 +0300

    Add files via upload

---
+++ Arranger_AdvancedFriendsUI/Localization.lua
@@ -0,0 +1,128 @@
+--
+-- Localization
+--
+
+local _, L = ...
+
+if GetLocale() == "deDE" then
+
+	-- --
+	-- -- German (Germany)
+	-- --
+
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_TRACK_TOOLTIP"] = "Achten Sie auf die Aktivitäten Ihres Freundes"
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_UNTRACK_TOOLTIP"] = "Höre auf zu schauen"
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_TRACK_MESSAGE"] = "Sie sehen jetzt nach: \"%s\""
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_UNTRACK_MESSAGE"] = "Du hast aufgehört, für \"%s\" zu sehen"
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE"] = "%s (%s%s-%s) hat die Spielwelt betreten."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE_HORDE"] = "%s (%s%s-%s) (Horde) hat die Spielwelt betreten."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE_ALLIANCE"] = "%s (%s%s-%s) (Alliance) hat die Spielwelt betreten."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_OFFLINE"] = "%s hat die Spielwelt verlassen."
+
+elseif GetLocale() == "enUS" or GetLocale() == "enGB" then
+
+	-- --
+	-- -- English (American) and English (Great Brittan)
+	-- --
+
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_TRACK_TOOLTIP"] = "Watch for your frind's activity"
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_UNTRACK_TOOLTIP"] = "Stop watching"
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_TRACK_MESSAGE"] = "You are now watching for: \"%s\""
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_UNTRACK_MESSAGE"] = "You've stopped watching for: \"%s\""
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE"] = "%s (%s%s-%s) has entered game world."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE_HORDE"] = "%s (%s%s-%s) has entered game world for the Horde."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE_ALLIANCE"] = "%s (%s%s-%s) has entered game world for the Alliance."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_OFFLINE"] = "%s has left game world."
+
+elseif GetLocale() == "esES" then
+
+	-- --
+	-- -- Spanish (Spain)
+	-- --
+
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_TRACK_TOOLTIP"] = "Ver la actividad de tu amigo"
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_UNTRACK_TOOLTIP"] = "Deja de mirar"
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_TRACK_MESSAGE"] = "Ahora estás viendo a \"%s\""
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_UNTRACK_MESSAGE"] = "Has dejado de mirar por \"%s\""
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE"] = "%s (%s%s-%s) ha entrado en el mundo del juego."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE_HORDE"] = "%s (%s%s-%s) (Horde) ha entrado en el mundo del juego."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE_ALLIANCE"] = "%s (%s%s-%s) (Alliance) ha entrado en el mundo del juego."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_OFFLINE"] = "%s ha dejado el mundo del juego."
+
+elseif GetLocale() == "esMX" then
+
+	-- --
+	-- -- Spanish (Mexico)
+	-- --
+
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_TRACK_TOOLTIP"] = "Ver la actividad de tu amigo"
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_UNTRACK_TOOLTIP"] = "Deja de mirar"
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_TRACK_MESSAGE"] = "Ahora estás viendo a \"%s\""
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_UNTRACK_MESSAGE"] = "Has dejado de mirar por \"%s\""
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE"] = "%s (%s%s-%s) ha entrado en el mundo del juego."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE_HORDE"] = "%s (%s%s-%s) (Horde) ha entrado en el mundo del juego."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE_ALLIANCE"] = "%s (%s%s-%s) (Alliance) ha entrado en el mundo del juego."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_OFFLINE"] = "%s ha dejado el mundo del juego."
+
+elseif GetLocale() == "frFR" then
+
+	-- --
+	-- -- French (France)
+	-- --
+
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_TRACK_TOOLTIP"] = "Surveillez l'activité de votre ami"
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_UNTRACK_TOOLTIP"] = "Arrêtez de regarder"
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_TRACK_MESSAGE"] = "Vous regardez maintenant \"%s\""
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_UNTRACK_MESSAGE"] = "Vous avez cessé de regarder \"%s\""
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE"] = "%s (%s%s-%s) est entré dans le monde du jeu."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE_HORDE"] = "%s (%s%s-%s) (Horde) est entré dans le monde du jeu."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE_ALLIANCE"] = "%s (%s%s-%s) (Alliance) est entré dans le monde du jeu."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_OFFLINE"] = "%s a quitté le monde du jeu."
+
+elseif GetLocale() == "itIT" then
+
+	-- --
+	-- -- Italian (Italy)
+	-- --
+
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_TRACK_TOOLTIP"] = "Guarda l'attività del tuo amico"
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_UNTRACK_TOOLTIP"] = "Fermati di guardare"
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_TRACK_MESSAGE"] = "Ora stai guardando \"%s\""
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_UNTRACK_MESSAGE"] = "Hai smesso di guardare \"%s\""
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE"] = "%s (%s%s-%s) è entrata nel mondo del gioco."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE_HORDE"] = "%s (%s%s-%s) (Horde) è entrata nel mondo del gioco."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE_ALLIANCE"] = "%s (%s%s-%s) (Alliance) è entrata nel mondo del gioco."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_OFFLINE"] = "%s ha lasciato il mondo del gioco."
+
+elseif GetLocale() == "ptBR" then
+	
+
+	-- --
+	-- -- Portuguese (Brazil)
+	-- --
+
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_TRACK_TOOLTIP"] = "Observe a atividade do seu amigo"
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_UNTRACK_TOOLTIP"] = "Pare de assistir"
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_TRACK_MESSAGE"] = "Você está olhando agora para \"%s\""
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_UNTRACK_MESSAGE"] = "Você parou de assistir \"%s\""
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE"] = "%s (%s%s-%s) entrou no mundo do jogo."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE_HORDE"] = "%s (%s%s-%s) (Horde) entrou no mundo do jogo."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE_ALLIANCE"] = "%s (%s%s-%s) (Alliance) entrou no mundo do jogo."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_OFFLINE"] = "%s deixou o mundo do jogo."
+
+elseif GetLocale() == "ruRU" then
+
+	-- --
+	-- -- Russian (Russia)
+	-- --
+
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_TRACK_TOOLTIP"] = "Отслеживать"
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_UNTRACK_TOOLTIP"] = "Прекратить отслеживание"
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_TRACK_MESSAGE"] = "Вы следите за: \"%s\""
+	L["ADVANCED_INTERFACE_FRIENDS_FRAME_UNTRACK_MESSAGE"] = "Вы прекратили следить за: \"%s\""
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE"] = "%s (%s%s-%s) входит в игровой мир."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE_HORDE"] = "%s (%s%s-%s) входит в игровой мир за Орду."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_ONLINE_ALLIANCE"] = "%s (%s%s-%s) входит в игровой мир за Альянс."
+	L["ADVANCED_INTERFACE_FRIEND_TRACKING_OFFLINE"] = "%s выходит из игрового мира."
+
+end
